76e689eaaccb41240c8ed039a837f3fbb98f1868
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-11-23 14:07:27 +0530

    Adding totalAmount field adding in Order Schema

--- schema/order.js
+++ schema/order.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { type } from "os";
 const OrderItemsSchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: "Products", required: true},
   name : String,
   quantity: { type: Number, required: true, min: 1 },
   price: {type: Number, required: true},
+  totalAmount: {type: Number, required: true}
 });
 const Order = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User_Admin", required: true},
